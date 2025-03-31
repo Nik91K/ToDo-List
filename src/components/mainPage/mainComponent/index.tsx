0a3897ed4d342a580c537c7dc4b43c374984ef55
Nikita <nikotakovsl@gmail.com>
2025-03-31 20:19:51 +0300

    added delete button

--- src/components/mainPage/mainComponent/index.tsx
+++ src/components/mainPage/mainComponent/index.tsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 import { TodoType } from "../../../types/dataTypes";
 import './index.css';
 
-const MainComponent = ({ tasks }: { tasks: TodoType[] }) => {
+const MainComponent = ({ tasks: deleteTask }: { tasks: TodoType[] }) => {
+    const [tasks, setTasks] = useState<TodoType[]>(deleteTask)
     const [checkedTasks, setCheckboxComplete] = useState<{ [key: number]: boolean }>({});
     const checkboxComplete = (taskId: number) => {
         setCheckboxComplete((prev) => ({
-            ...prev,
-            [taskId]: !prev[taskId],
-        }));
-    };
+            ...prev,[taskId]: !prev[taskId],
+        }))
+    }
 
+    function handleDelete(taskId: number) {
+        console.log('завдання Ід видалено', taskId)
+        setTasks(tasks.filter((t) => t.id !== taskId))
+    }
+    
     return (
         <ul className="task-list">
             {tasks.map((task) => (
@@ -21,14 +26,17 @@ const MainComponent = ({ tasks }: { tasks: TodoType[] }) => {
                     <input 
                     type="checkbox"
                     onChange={() => checkboxComplete(task.id)}
-                    checked={!!checkedTasks[task.id]}
+                    checked={checkedTasks[task.id]}
                     className="checkbox-task-list"
                     />
                     {task.name}
+                    <span className="delete-task-list">
+                        <button onClick={() => handleDelete(task.id)}>x</button>
+                    </span>
                 </li>
             ))}
         </ul>
-    );
-};
+    )
+}
 
 export default MainComponent;
